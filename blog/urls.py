f83e0fcfc5041e33164f2bec45144bcb57d2eb45
Rashmi Singh1 <Rashmi1.Singh@lntinfotech.com>
2021-07-04 21:17:57 +0530

    First commit

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.starting_page, name="starting-page"),
+    path("posts/", views.posts, name="all-posts"),
+    path("posts/<slug:post_slug>", views.post_detail, name="selected-post"),
+    path("about/",views.about,name="about"),
+    path("policy/",views.policy,name="policy"),
+    path("carrers/",views.carrers,name="carrers")
+]
